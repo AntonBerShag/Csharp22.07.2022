f43fe74fcdbeacc8ef9f224c7d2317f24d784004
Владислав Цыбарт <tsybart_v@itstep.academy>
2022-07-22 21:09:16 +0500

    Добавьте файлы проекта.

---
+++ Csharp22.07.2022/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Csharp22._07._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Uchastniki[] uchasniki = new Uchastniki[3];
+            List<Uchastniki> uchasniki = new List<Uchastniki>();
+
+            int count = Uchastniki.Enter_age("Введите кол-во участников: ");
+            for (int i = 0; i < count; i++)
+            {
+                uchasniki.Add(new Uchastniki(i + 1));
+            }
+
+            void vyvod()
+            {
+                Console.WriteLine("Все участники: ");
+
+                foreach (var item in uchasniki)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+
+            Console.WriteLine("[1] Добавить участника\n[2] Удалить участника");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    {
+                        uchasniki.Add(new Uchastniki(uchasniki.Count + 1));
+                        /*Array.Resize(ref uchasniki, uchasniki.Length + 1);
+                        uchasniki[uchasniki.Length - 1] = new Uchastniki(uchasniki.Length);*/
+                        vyvod();
+                        break;
+                    }
+                case "2":
+                    {
+                        vyvod();
+                        uchasniki.RemoveAt(Uchastniki.Enter_age("Введите id:") - 1);
+                        vyvod();
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ Csharp22.07.2022/Uchastniki.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp22._07._2022
+{
+    //Участники соревнований по спорт. орентированию
+    class Uchastniki
+    {
+        string Enter(string text)
+        {
+            Console.Write(text);
+            return Console.ReadLine();
+        }
+
+        public static int Enter_age(string text)
+        {
+            try
+            {
+                Console.Write(text);
+                return int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка ввода!");
+                return Enter_age(text);
+            }
+        }
+
+        public Uchastniki(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
+        public Uchastniki(int id)
+        {
+            Id = id;
+            Name = Enter("Введите ФИО участника: ");
+            Age = Enter_age("Введите возраст участника: ");
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID Участника: {Id}, \nФИО Участника: {Name}\nВозраст Участника: {Age}";
+        }
+    }
+}
